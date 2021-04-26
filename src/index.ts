cec1d80a19347ecbbfc0d80d7d0ff97f22e1b477
Deep Patel <deepp0925@gmail.com>
2021-04-26 12:15:01 -0400

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,20 @@ import { connect } from "mongoose";
 import express from "express";
 import { coursesRouter, topicsRouter, userRouter } from "./routes";
 import { json, urlencoded } from "body-parser";
-import cors from "cors";
-import helmet from "helmet";
-import { resolve } from "path";
+import { join } from "path";
 
 const PORT = process.env.PORT || 8080;
 
 // setting up express app
 const app = express();
-app.use(cors());
-app.use(helmet());
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use("/user", userRouter);
 app.use("/topic", topicsRouter);
 app.use("/course", coursesRouter);
 // is in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../../views/build"));
 
-  app.get("*", resolve(__dirname, "views", "build", "index.html") as any);
-}
+app.use("/", express.static(join(__dirname, "..", "..", "views", "build")));
 
 // starts the express app and connects to mongodb
 app.listen(PORT, () => {
